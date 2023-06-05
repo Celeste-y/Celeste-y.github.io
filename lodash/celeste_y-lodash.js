d2a5312b019506a5172478aa0b6e9eeb72fc4cc3
Celeste <1098245608@qq.com>
2023-06-05 10:02:58 +0800

    okey

--- lodash/celeste_y-lodash.js
+++ lodash/celeste_y-lodash.js
@@ -17,5 +17,28 @@ var celeste_y = {
     return result
   },
 
+  concat : function (array,...val){
+    var result = array
+    for(var i = 0;i<val.length;i++){
+      if(Array.isArray(val[i])){
+        for(var j = 0;j<val[i].length;j++){
+          result.push(val[i][j])
+        }
+      }else{
+        result.push(val[i])
+      }
+    }
+    return result
+  },
+
+  difference : function (array,...val){
+    val = this.flattenDeep(val)
+    for(var i=0;i<val.length ;i++){
+      var a = array.filter(it => it !== val[i])
+      array = a
+    }
+     return a
+  },
+
   
 }
