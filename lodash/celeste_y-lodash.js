98667117926b84b150d66892ceb3fa8bbb9655cd
Celeste <1098245608@qq.com>
2023-06-06 20:22:05 +0800

    ok

--- lodash/celeste_y-lodash.js
+++ lodash/celeste_y-lodash.js
@@ -43,5 +43,85 @@ var celeste_y = {
     }
     return ary
   },
-  
+  findIndex: function(ary, test, fromIndex = 0){
+    for(i = fromIndex; i < ary.length; i++){
+      if(typeof(test) == 'function'){
+        if(test(ary[i]) == true){
+          return i
+        }
+      }
+      if(typeof(test) == 'object'){
+        if(Array.isArray(test)){
+          if(test[0] in ary[i] && test[1] == ary[i][test[0]]){
+            return i
+          }
+        }else{
+            let c = true
+            for(let item in test){
+              if(ary[i][item] !== test[item]){
+                c = false
+              }
+            }
+            if(c){
+              return i
+            }
+        }
+      }
+      if(typeof(test) == 'string'){
+        if(test in ary[i] && ary[i][test]){
+          return i
+        }
+      }
+    }
+    return -1
+  },
+  findLastIndex: function(ary, test, fromIndex = ary.length - 1){
+    for(i = fromIndex; i >= 0; i--){
+      if(typeof(test) == 'function'){
+        if(test(ary[i]) == true){
+          return i
+        }
+      }
+      if(typeof(test) == 'object'){
+        if(Array.isArray(test)){
+          if(test[0] in ary[i] && test[1] == ary[i][test[0]]){
+            return i
+          }
+        }else{
+            let c = true
+            for(let item in test){
+              if(ary[i][item] !== test[item]){
+                c = false
+              }
+            }
+            if(c){
+              return i
+            }
+        }
+      }
+      if(typeof(test) == 'string'){
+        if(test in ary[i] && ary[i][test]){
+          return i
+        }
+      }
+    }
+    return -1
+  },
+  flatten: function(ary){
+    return ary.flat()
+  },
+  flattenDeep: function(ary){
+    for(i = 0; i < ary.length; i++){
+      if(Array.isArray(ary[i])){
+        ary = ary.flat()
+      }
+    }
+    return ary
+  },
+  flattenDepth: function(ary, n){
+    for(i = 0; i < n; i++){
+      ary = ary.flat()
+    }
+    return ary
+  },
 }
